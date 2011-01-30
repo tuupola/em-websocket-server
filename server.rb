1b11088cebf8fb8eefc4847979d5da987fa02382
Mika Tuupola <tuupola@appelsiini.net>
2011-01-30 22:59:12 +0200

    Send server messages as JSON.

--- server.rb
+++ server.rb
@@ -6,30 +6,33 @@ EM.run do
   
   @channels = Hash.new
 
-  EM::PeriodicTimer.new(10) do
-    @channels.each do |path, channel|
-      channel.push  "#{path} The time is now #{Time.now}".to_json
-    end
-  end
+  #EM::PeriodicTimer.new(10) do
+  #  @channels.each do |path, channel|
+  #    channel.push  "#{path} The time is now #{Time.now}".to_json
+  #  end
+  #end
   
   EM::WebSocket.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |socket|
   
     socket.onopen do
+
+      payload = Hash.new
     
       sid = channel_for_socket(socket).subscribe do |message| 
         socket.send message 
       end
       
-      channel_for_socket(socket).push "#{sid} connected!".to_json
+      puts payload[:server] = "#{sid} connected."
+      channel_for_socket(socket).push payload.to_json
       
       socket.onmessage do |data| 
         channel_for_socket(socket).push data
       end
 
       socket.onclose do
-        channel_for_socket(socket).push "#{sid} disconnected!"
         channel_for_socket(socket).unsubscribe(sid)
-        puts "#{sid} closed socket." 
+        puts payload[:server] = "#{sid} disconnected."
+        channel_for_socket(socket).push payload.to_json
       end
     
     end
@@ -44,10 +47,7 @@ EM.run do
 
   def channel_for_socket(socket)
     path = socket.request["Path"].downcase
-    if @channels[path].nil? 
-      @channels[path] = EM::Channel.new
-    end
-    @channels[path]
+    @channels[path] ||= EM::Channel.new
   end
   
 end
