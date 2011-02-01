9e46fee1ff447ab499823ea2105bb592618311c7
Mika Tuupola <tuupola@appelsiini.net>
2011-02-01 11:07:37 +0200

    Ruby 1.8.6 does not have count. Use size instead.

--- server.rb
+++ server.rb
@@ -20,9 +20,9 @@ EM.run do
   EM::PeriodicTimer.new(10) do
     @subscribers = 0
     @channels.each do |path, channel|
-      @subscribers += channel.subs.count
+      @subscribers += channel.subs.size
     end
-    @log.debug "Channels #{@channels.count}, subscribers #{@subscribers}, messages per second #{@messages / 10}."
+    @log.debug "Channels #{@channels.size}, subscribers #{@subscribers}, messages per second #{@messages / 10}."
     @messages    = 0
   end
   
@@ -40,7 +40,7 @@ EM.run do
       channel_for_socket(socket).push payload.to_json
       socket.onmessage do |data| 
         channel_for_socket(socket).push data
-        @messages += channel_for_socket(socket).subs.count
+        @messages += channel_for_socket(socket).subs.size
       end
 
       socket.onclose do
